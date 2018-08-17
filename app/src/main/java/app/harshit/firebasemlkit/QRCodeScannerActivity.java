9c474cb81bd233f2c9a0171def5fc5e4b7c53063
the-dagger <harshithdwivedi@gmail.com>
2018-08-18 03:20:49 +0530

    complete QR code scanner
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/app/harshit/firebasemlkit/QRCodeScannerActivity.java
+++ app/src/main/java/app/harshit/firebasemlkit/QRCodeScannerActivity.java
@@ -3,10 +3,22 @@ package app.harshit.firebasemlkit;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.otaliastudios.cameraview.CameraListener;
+
+import java.util.List;
 
 public class QRCodeScannerActivity extends BaseCameraActivity {
 
@@ -23,11 +35,34 @@ public class QRCodeScannerActivity extends BaseCameraActivity {
     }
 
     private void getCodeFromImage(Bitmap bitmap) {
-        //TODO : Create a FirebaseVisionImage
-
-        //TODO : Get access to a FirebaseImageDetector
-
-        //TODO : Use the detector to detect the labels inside the image
+        //Create a FirebaseVisionImage
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        //Get access to a FirebaseBarcodeDetector
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector();
+        //Use the detector to detect the QR code inside the image
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                        textView.setText("");
+                        for (FirebaseVisionBarcode barcode : firebaseVisionBarcodes) {
+                            textView.append(barcode.getDisplayValue() + "\n");
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getBaseContext(), "Failed to read QR code", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<FirebaseVisionBarcode>> task) {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 
     @Override
