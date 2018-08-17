30c4507f3736ec0eb9c1bf7eae4a9181d8000957
the-dagger <harshithdwivedi@gmail.com>
2018-08-18 03:07:57 +0530

    finish image labeling
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/app/harshit/firebasemlkit/BaseCameraActivity.java
+++ app/src/main/java/app/harshit/firebasemlkit/BaseCameraActivity.java
@@ -1,6 +1,44 @@
 package app.harshit.firebasemlkit;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewStub;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class BaseCameraActivity extends AppCompatActivity {
+import com.otaliastudios.cameraview.CameraView;
+
+//base camera activity to handle camera preview and other things
+public abstract class BaseCameraActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CameraView cameraView;
+    Button btnCapture;
+    TextView textView;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_camera);
+        cameraView = findViewById(R.id.cameraView);
+        btnCapture = findViewById(R.id.btnCapture);
+        textView = findViewById(R.id.tView);
+        btnCapture.setOnClickListener(this);
+        progressBar = findViewById(R.id.pb);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cameraView.start();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        cameraView.stop();
+    }
 }
--- app/src/main/java/app/harshit/firebasemlkit/ImageLabelActivity.java
+++ app/src/main/java/app/harshit/firebasemlkit/ImageLabelActivity.java
@@ -1,4 +1,79 @@
 package app.harshit.firebasemlkit;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabel;
+import com.google.firebase.ml.vision.cloud.label.FirebaseVisionCloudLabelDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionLabelDetector;
+import com.otaliastudios.cameraview.CameraListener;
+
+import java.util.List;
+
 public class ImageLabelActivity extends BaseCameraActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        cameraView.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] jpeg) {
+                //Convert ByteArray to a Bitmap
+                getLabelFromCloud(BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length));
+            }
+        });
+    }
+
+    private void getLabelFromDevice(Bitmap bitmap) {
+
+        //Create a FirebaseVisionImage
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        //Get access to a FirebaseImageDetector
+        FirebaseVisionLabelDetector detector = FirebaseVision.getInstance()
+                .getVisionLabelDetector();
+
+        //Use the detector to detect the labels inside the image
+        detector.detectInImage(image).addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionLabel>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionLabel> firebaseVisionLabels) {
+                for (FirebaseVisionLabel firebaseVisionLabel : firebaseVisionLabels) {
+                    progressBar.setVisibility(View.GONE);
+                    //Get the label and its confidence
+                    textView.append(firebaseVisionLabel.getLabel() + "  :  " + firebaseVisionLabel.getConfidence() * 100 + "% \n");
+                }
+            }
+        });
+    }
+
+    void getLabelFromCloud(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionCloudLabelDetector detector = FirebaseVision.getInstance()
+                .getVisionCloudLabelDetector();
+
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLabel>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionCloudLabel> firebaseVisionCloudLabels) {
+                        textView.setText("");
+                        progressBar.setVisibility(View.GONE);
+                        for (FirebaseVisionCloudLabel firebaseVisionLabel : firebaseVisionCloudLabels) {
+                            textView.append(firebaseVisionLabel.getLabel() + "  :  " + firebaseVisionLabel.getConfidence() * 100 + "% \n");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onClick(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        cameraView.capturePicture();
+    }
 }
