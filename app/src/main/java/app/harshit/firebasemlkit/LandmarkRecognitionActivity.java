bf17a39477004e68f53f221c2578d1923800ccce
the-dagger <harshithdwivedi@gmail.com>
2018-08-18 11:27:59 +0530

    finish landmark recognition
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/app/harshit/firebasemlkit/LandmarkRecognitionActivity.java
+++ app/src/main/java/app/harshit/firebasemlkit/LandmarkRecognitionActivity.java
@@ -2,11 +2,25 @@ package app.harshit.firebasemlkit;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmark;
+import com.google.firebase.ml.vision.cloud.landmark.FirebaseVisionCloudLandmarkDetector;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionLatLng;
 import com.otaliastudios.cameraview.CameraListener;
+
+import java.util.List;
 
 public class LandmarkRecognitionActivity extends BaseCameraActivity {
 
@@ -23,11 +37,39 @@ public class LandmarkRecognitionActivity extends BaseCameraActivity {
     }
 
     private void getLandmarkFromImage(Bitmap bitmap) {
-        //TODO : Create a FirebaseVisionImage
+        //Create a FirebaseVisionImage
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        //Get access to a FirebaseCloudVisionLandmarkDetector
+        FirebaseVisionCloudLandmarkDetector detector = FirebaseVision.getInstance()
+                .getVisionCloudLandmarkDetector();
+        //Use the detector to detect the landmarks inside the image
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionCloudLandmark>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionCloudLandmark> firebaseVisionCloudLandmarks) {
+                        textView.setText("");
+                        for (FirebaseVisionCloudLandmark landmark : firebaseVisionCloudLandmarks) {
 
-        //TODO : Get access to a FirebaseCloudVisionLandmarkDetector
+                            textView.append("Landmark name  : " + landmark.getLandmark());
+                            for (FirebaseVisionLatLng location : landmark.getLocations()) {
+                                textView.append("Latitude : " + location.getLatitude());
+                                textView.append("Longitude : " + location.getLongitude());
+                            }
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LandmarkRecognitionActivity.this, "There was an error", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<List<FirebaseVisionCloudLandmark>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<FirebaseVisionCloudLandmark>> task) {
 
-        //TODO : Use the detector to detect the landmarks inside the image
+                    }
+                });
     }
 
     @Override
