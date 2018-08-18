fd85bac61d2fa155a291c9e7738b7de197c3884f
the-dagger <harshithdwivedi@gmail.com>
2018-08-18 11:03:22 +0530

    complete face detection
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

---
+++ app/src/main/java/app/harshit/firebasemlkit/FaceDetectionActivity.java
@@ -0,0 +1,75 @@
+package app.harshit.firebasemlkit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.face.FirebaseVisionFace;
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetector;
+import com.otaliastudios.cameraview.CameraListener;
+import com.otaliastudios.cameraview.Facing;
+
+import java.util.List;
+
+public class FaceDetectionActivity extends BaseCameraActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        cameraView.setFacing(Facing.FRONT);
+        cameraView.addCameraListener(new CameraListener() {
+            @Override
+            public void onPictureTaken(byte[] jpeg) {
+                //Convert ByteArray to a Bitmap
+                getFaceFromImage(BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length));
+            }
+        });
+    }
+
+    private void getFaceFromImage(Bitmap bitmap) {
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionFaceDetector detector = FirebaseVision.getInstance().getVisionFaceDetector();
+
+        detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionFace>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionFace> firebaseVisionFaces) {
+                        textView.setText("");
+                        for (FirebaseVisionFace firebaseVisionFace : firebaseVisionFaces) {
+                            textView.append("Right eye Open probability: " + firebaseVisionFace.getRightEyeOpenProbability());
+                            textView.append("Left eye Open probability: " + firebaseVisionFace.getLeftEyeOpenProbability());
+                            textView.append("Smiling probability " + firebaseVisionFace.getSmilingProbability());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(FaceDetectionActivity.this, "An error occured", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnCompleteListener(new OnCompleteListener<List<FirebaseVisionFace>>() {
+                    @Override
+                    public void onComplete(@NonNull Task<List<FirebaseVisionFace>> task) {
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        progressBar.setVisibility(View.VISIBLE);
+        cameraView.capturePicture();
+    }
+}
