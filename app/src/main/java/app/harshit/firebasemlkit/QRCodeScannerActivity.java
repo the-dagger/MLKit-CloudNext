6de139129647debcbb52771851004f45a86e1f77
the-dagger <harshithdwivedi@gmail.com>
2018-08-18 12:54:22 +0530

    qr code finish
    Signed-off-by: the-dagger <harshithdwivedi@gmail.com>

--- app/src/main/java/app/harshit/firebasemlkit/QRCodeScannerActivity.java
+++ app/src/main/java/app/harshit/firebasemlkit/QRCodeScannerActivity.java
@@ -1,10 +1,7 @@
 package app.harshit.firebasemlkit;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,23 +13,12 @@ import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
 import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.otaliastudios.cameraview.CameraListener;
+import com.wonderkiln.camerakit.CameraKitEventCallback;
+import com.wonderkiln.camerakit.CameraKitImage;
 
 import java.util.List;
 
 public class QRCodeScannerActivity extends BaseCameraActivity {
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        cameraView.addCameraListener(new CameraListener() {
-            @Override
-            public void onPictureTaken(byte[] jpeg) {
-                //Convert ByteArray to a Bitmap
-                getCodeFromImage(BitmapFactory.decodeByteArray(jpeg, 0, jpeg.length));
-            }
-        });
-    }
 
     private void getCodeFromImage(Bitmap bitmap) {
         //Create a FirebaseVisionImage
@@ -68,6 +54,11 @@ public class QRCodeScannerActivity extends BaseCameraActivity {
     @Override
     public void onClick(View view) {
         progressBar.setVisibility(View.VISIBLE);
-        cameraView.capturePicture();
+        cameraView.captureImage(new CameraKitEventCallback<CameraKitImage>() {
+            @Override
+            public void callback(CameraKitImage cameraKitImage) {
+                getCodeFromImage(cameraKitImage.getBitmap());
+            }
+        });
     }
 }
